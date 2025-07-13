3441c0e3f1ce4c20aaa6b7a054995ff849808c73
x_jaima <jaime.manriquez@math.lth.se>
2025-07-13 22:44:48 +0200

    fixed bash scripts

--- experiments/blobs_close.sh
+++ experiments/blobs_close.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -n 40
+#SBATCH -n 30
 #SBATCH -t 48:00:00
 #SBATCH -J BLOB_CLOSE
 #SBATCH -o output/slurm-%j.out
@@ -27,31 +27,68 @@ TIME_SNAP=1E-03
 ABS_TOL=1E-9
 REL_TOL=1E-8
 
-Q_MAX_INFLOW=0.0
-S_INFLOW=0.0
-
-run_parameters="
---name=${NAME} --output_dir=${OUTPUT_DIR}
---filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA}
---inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER}
---preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER}
---time_step_snap=${TIME_SNAP}
---abs_tol=${ABS_TOL} --rel_tol=${REL_TOL}
---n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP}
-"
-
 # Activate FEniCS environment
 module load buildtool-easybuild/4.8.0-hpce082752a2
 module load Miniforge/24.7.1-2-hpc1
 conda activate fenicsproject
-ssf="-n 1 -N 1 --exclusive python main.py"
-for N in "${N_list[@]}"; do
-    srun ${ssf} --name=zero_rx_${NAME} ${run_parameters} --c_1=0.0 --c_2=0.0 &; sleep 1
-    srun ${ssf} --name=norm_rx_${NAME} ${run_parameters} --c_1=1E2 --c_2=1E3 &; sleep 1
-    srun ${ssf} --name=high_rx_${NAME} ${run_parameters} --c_1=1E3 --c_2=1E4 &; sleep 1
 
-    srun ${ssf} --name=zero_rx_${NAME}_gamma1 ${run_parameters} -G=1 --c_1=0.0 --c_2=0.0 &; sleep 1
-    srun ${ssf} --name=norm_rx_${NAME}_gamma1 ${run_parameters} -G=1 --c_1=1E2 --c_2=1E3 &; sleep 1
-    srun ${ssf} --name=high_rx_${NAME}_gamma1 ${run_parameters} -G=1 --c_1=1E3 --c_2=1E4 &; sleep 1
-done
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=zero_rx_${NAME} \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+--c_1=0.0 --c_2=0.0 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=norm_rx_${NAME} \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+--c_1=1E2 --c_2=1E3 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=high_rx_${NAME} \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+--c_1=1E3 --c_2=1E4 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=zero_rx_${NAME}_gamma1 \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+-G=1 --c_1=0.0 --c_2=0.0 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=norm_rx_${NAME}_gamma1 \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+-G=1 --c_1=1E2 --c_2=1E3 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=high_rx_${NAME}_gamma1 \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+-G=1 --c_1=1E3 --c_2=1E4 & sleep 1
 wait
--- experiments/ssf.sh
+++ experiments/ssf.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-#SBATCH -n 40
+#SBATCH -n 20
 #SBATCH -t 48:00:00
 #SBATCH -J SSF_2D
 #SBATCH -o output/slurm-%j.out
@@ -30,26 +30,40 @@ REL_TOL=1E-8
 Q_MAX_INFLOW=300.0
 S_INFLOW=988.02 # 0.99 * 998
 
-run_parameters="
---name=${NAME} --output_dir=${OUTPUT_DIR}
---filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA}
---inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER}
---preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER}
---inflow_velocity=${Q_MAX_INFLOW} --inflow_profile=parabolic
---inflow_concentration=${S_INFLOW}
---time_step_snap=${TIME_SNAP}
---abs_tol=${ABS_TOL} --rel_tol=${REL_TOL}
---n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP}
-"
-
 # Activate FEniCS environment
 module load buildtool-easybuild/4.8.0-hpce082752a2
 module load Miniforge/24.7.1-2-hpc1
 conda activate fenicsproject
-ssf="-n 1 -N 1 --exclusive python main.py"
-for N in "${N_list[@]}"; do
-    srun ${ssf} --name=zero_rx_${NAME} ${run_parameters} --c_1=0.0 --c_2=0.0 &; sleep 1
-    srun ${ssf} --name=norm_rx_${NAME} ${run_parameters} --c_1=2E1 --c_2=1E3 &; sleep 1
-    srun ${ssf} --name=high_rx_${NAME} ${run_parameters} --c_1=1E3 --c_2=5E3 &; sleep 1
-done
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=zero_rx_${NAME} \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--inflow_velocity=${Q_MAX_INFLOW} --inflow_profile=parabolic --inflow_concentration=${S_INFLOW} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+--c_1=0.0 --c_2=0.0 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=norm_rx_${NAME} \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--inflow_velocity=${Q_MAX_INFLOW} --inflow_profile=parabolic --inflow_concentration=${S_INFLOW} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+--c_1=2E1 --c_2=1E3 & sleep 1
+
+srun --ntasks=1 --nodes=1 --exclusive python main.py --name=high_rx_${NAME} \
+--output_dir=${OUTPUT_DIR} \
+--filter_length=${FILTER_LENGTH} --filter_area=${FILTER_AREA} \
+--inlet_area=${INLET_AREA} --inlet_length=${INLET_LENGTH} --inlet_center=${INLET_CENTER} \
+--preset_name=${PRESET} --preset_parameter=${PRESET_PARAMETER} \
+--inflow_velocity=${Q_MAX_INFLOW} --inflow_profile=parabolic --inflow_concentration=${S_INFLOW} \
+--time_step_snap=${TIME_SNAP} \
+--abs_tol=${ABS_TOL} --rel_tol=${REL_TOL} \
+--n_mesh=${N_ELEMENTS} --n_steps=${N_STEPS} --time_step_size=${TIME_STEP} \
+--c_1=1E3 --c_2=5E3 & sleep 1
 wait
