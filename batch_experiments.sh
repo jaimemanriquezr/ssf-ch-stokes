00c5b5f34fd1819cfe6c635673b7d544a3864b31
x_jaima <jaime.manriquez@math.lth.se>
2025-07-16 09:39:24 +0200

    added n_steps as input for bash script of experiment batching

--- batch_experiments.sh
+++ batch_experiments.sh
@@ -3,9 +3,10 @@ if [[ "$(whoami)" == *"tetralith"* ]]; then
     module load buildtool-easybuild/4.8.0-hpce082752a2
     module load ParaView/5.13.1-hpc1-bdist
 fi
-DIR=${1:-./experiments/}
+N_STEPS=${1:-10}
+DIR=${2:-./experiments/}
 
 for filename in "$DIR"*.sh; do
     echo "Batching ${filename}"
-    sbatch "${filename}"
+    sbatch "${filename}" "${N_STEPS}"
 done
